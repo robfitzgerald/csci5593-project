75a32b5bdc47a0c1f9f7f6dd0755dc5d55c417cc
Rob Fitzgerald <robfitzgerald@gmail.com>
2017-04-26 21:49:57 -0600

    baseline script generator

---
+++ src/edu/colorado/cse/csci5593/team8/BaselineScriptGenerator.scala
@@ -0,0 +1,28 @@
+package edu.colorado.cse.csci5593.team8
+
+import java.io.PrintWriter
+
+object BaselineScriptGenerator extends App {
+  if (args.size < 2) {
+    println("usage: <absolute/path/to/ExecutableBinary> <destFile>")
+  } else {
+    val binPath: String = args(0)
+    val printWriter: PrintWriter = new PrintWriter(args(1))
+    val iterations: Integer = if (args.size > 2) args(2).toInt else 1000
+    
+    (2 to 17).combinations(2).foreach(pair => {
+        // setup interpolated values
+        val fwdNodeList: String = pair.mkString(",")
+        val fwdTestName: String = s"${pair(0)}-${pair(1)}"
+        val revNodeList: String = pair.reverse.mkString(",")
+        val revTestName: String = s"${pair(1)}-${pair(0)}"
+        // generate shell script lines for this pair
+        val fwdScript: String = s"""srun --mpi=pmi2 -n2 -w "node[$fwdNodeList]" $binPath $fwdTestName $iterations 1"""
+        val revScript: String = s"""srun --mpi=pmi2 -n2 -w "node[$revNodeList]" $binPath $revTestName $iterations 1"""
+        printWriter.println(fwdScript)
+        printWriter.println(revScript)
+      })
+
+    printWriter.close ();  
+  }
+}
